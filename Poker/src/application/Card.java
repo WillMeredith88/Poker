3144569d2f1d72e6e4a906521b26f2ef464ff2ff
Matt <Matt@172.24.144.176>
2018-10-06 13:09:51 -0500

    added card, hand, board classes

--- Poker/src/application/Card.java
+++ Poker/src/application/Card.java
@@ -1,15 +1,104 @@
 package application;
 
+import java.util.Random;
+
 public class Card {
-/**
- * Data fields
- */
+	/**
+	 * Instance Variable(s)
+	 */
+	private int value;
+	private char suit;
 	
 	/**
-	 * Constructors
+	 * Constructor(s)
 	 */
+	public Card(){//default constructor
+		this(1, 'c');
+	}
+	//value(1- 13)
+	public Card(int value, char suit){
+		this.value = value;
+		this.suit = suit;
+	}
 	
 	/**
-	 * Methods
+	 * Instance Method(s)
 	 */
+
+	public int getValue() {
+		return this.value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public char getSuit() {
+		return this.suit;
+	}
+
+	public void setSuit(char suit) {
+		this.suit = suit;
+	}
+	
+	public static int randomValue(){
+		Random gen = new Random();
+		return gen.nextInt(13) + 1;
+	}
+	
+	public static char randomSuit(){
+		Random gen = new Random();
+		int ranInt = gen.nextInt(4);
+		char ch;
+		
+		if(ranInt == 0){
+			ch = 'c';//clubs
+		}else if(ranInt == 1){
+			ch = 'd';//diamonds
+		}else if(ranInt == 2){
+			ch = 'h';//hearts
+		}else{
+			ch = 's';//spades
+		}
+		return ch;
+	}
+	
+	public String valueToString(){
+		String val = "";
+//		if(this.value == 1){
+//			val += "Ace";
+//		}else if(this.value == 11){
+//			val += "Jack";
+//		}else if(this.value == 12){
+//			val += "Queen";
+//		}else if(this.value == 13){
+//			val += "King";
+//		}else{
+//			val += this.getValue();
+//		}
+		val += this.getValue();
+		return val;
+	}
+	
+	public String suitToString(){
+		String ste = "";
+		if(this.suit == 'c'){
+		//	ste += "Club's ";
+			ste += "c";
+		}else if(this.suit == 'd'){
+		//	ste += "Diamond's ";
+			ste += "d";
+		}else if(this.suit == 'h'){
+		//	ste += "Heart's ";
+			ste += "h";
+		}else if(this.suit == 's'){
+		//	ste += "Spade's ";
+			ste += "s";
+		}
+		return ste;
+	}
+	
+	public String toString(){
+		return  this.valueToString() + suitToString();
+	}
 }
--- Poker/src/application/Main.java
+++ Poker/src/application/Main.java
@@ -6,8 +6,8 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
-public class Main extends Application {
-	@Override
+public class Main{ //extends Application {
+	/*@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
@@ -18,9 +18,47 @@ public class Main extends Application {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-	}
+	}*/
 	
 	public static void main(String[] args) {
-		launch(args);
+		//launch(args);
+//		//value suit
+//		Card c1 = new Card();
+//		c1.setValue(6);
+		Card c1 = new Card(1, 'c');
+		Card c2 = new Card(13, 'c');
+		Card c3 = new Card(12, 'c');
+		Card c4 = new Card(11, 'c');
+		Card c5 = new Card(10, 'c');
+		Card c6 = new Card(6, 'd');
+		Card c7 = new Card(7, 'd');
+//		Hand h1 = new Hand(c6, c7);
+////		h1.setNext(c2);
+////		System.out.println(h1.toString());
+//		System.out.println();
+//		
+//		Board b1 = new Board(c1, c2, c3);
+//		b1.setNext(c4);
+//		b1.setNext(c5);
+////		System.out.println(b1.toString());
+//		System.out.println();
+		Hand h1 = new Hand();
+		//h1.randomHand();
+		h1.setHand(c1, c2);
+		Board b1 = new Board();
+		//b1.randomBoard();
+		b1.setBoard(c3, c4, c5, c6, c7);
+		System.out.println();
+		
+		GameCalc gc = new GameCalc(h1, b1);
+//		gc.setNext(c5);
+		System.out.println(gc.toString());
+		
+		gc.getHighCard();
+		System.out.println();
+		gc.checkPairs();
+		gc.checkFlush();
+		gc.checkStraight();
+		gc.checkStraightFlush();
 	}
 }
